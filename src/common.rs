6f00c9a3885aa767f0b42a7e1b6755082efe9ef9
era <me@elias.sh>
2025-03-23 17:38:12 +0900

    explaning this only works with 64b

--- src/common.rs
+++ src/common.rs
@@ -1,6 +1,9 @@
 use anyhow::{anyhow, Result};
 
 pub async fn read_bytes(receiver: &mut quinn::RecvStream) -> Result<Vec<u8>> {
+    // if this was a real software, we probably should use a i32, instead of usize
+    // since we would like to be compatible with both 32 and 64.
+    // Since this is just a demo, I just wanted the easy way out.
     let mut size = [0u8; 8];
 
     receiver
